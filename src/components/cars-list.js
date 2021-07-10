6085c74c3a2ae90573e209e98e3002da1a430b0f
Toshiyuki 120186 <toshiyuki120186@gmail.com>
2021-07-10 23:27:15 +0800

    loading center

--- src/components/cars-list.js
+++ src/components/cars-list.js
@@ -100,8 +100,10 @@ const CarsList = props => {
         </div>
       </div>
       {isLoading ? (
-        <div class="spinner-border" role="status">
-          <span class="sr-only">Loading...</span>
+        <div class="d-flex justify-content-center">
+          <div class="spinner-border" role="status">
+            <span class="sr-only">Loading...</span>
+          </div>
         </div>
       ) : (
         <div className="row">
